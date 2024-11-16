c45fb65bcb7a8590a744e2573a5af3d473776326
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2024-11-16 18:54:11 +0800

    ywetwtwtwetwetwe
    2nd rerrrrrrrrrrrrrrrrrk

--- script.js
+++ script.js
@@ -51,3 +51,16 @@ loginTag.addEventListener('click', () => {
 })
 
 
+let wave1 = document.getElementById('wave1');
+let wave2 = document.getElementById('wave2');
+let wave3 = document.getElementById('wave3');
+let wave4 = document.getElementById('wave4');
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY - 800;
+
+    wave1.style.backgroundPositionX = 1 + value * 1 + 'px';
+    wave2.style.backgroundPositionX = 2 + value * -1 + 'px';
+    wave3.style.backgroundPositionX = 1 + value * 3 + 'px';
+    wave4.style.backgroundPositionX = 2 + value * -2 + 'px';
+})
